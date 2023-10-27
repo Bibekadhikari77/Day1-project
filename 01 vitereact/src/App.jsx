640b2037d02f30184f32d0b1532e5da8f2749e24
bibek <adhikaribibek84@gmail.com>
2023-10-27 15:04:05 +0545

    first commit

---
+++ 01 vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from "./chai"
+function App() {
+  const username = "chai aur code"
+
+  return (
+    <>
+          <Chai/>
+          <h2>chai aur react {username}</h2>
+          <p> This is the first day project <b> React is the most powerful </b>first project </p>
+    </>
+    )
+}
+
+export default App
